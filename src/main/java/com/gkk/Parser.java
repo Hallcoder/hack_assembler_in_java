31bd81dfd2e6e883d97bbab0b02e21bf561fb3b9
Hallcoder <hallcoder25@gmail.com>
2025-07-01 09:58:57 +0200

    initial commit

---
+++ src/main/java/com/gkk/Parser.java
@@ -0,0 +1,11 @@
+package com.gkk;
+
+public class Parser {
+    // This class is responsible for parsing the input and generating the AST (Abstract Syntax Tree)
+    // It will handle the syntax of the language and create a structured representation of the code
+
+    public void parse(String input) {
+
+        System.out.println("Parsing input: " + input);
+    }
+}
---
+++ src/main/java/com/gkk/SymbolTable.java
@@ -0,0 +1,4 @@
+package com.gkk;
+
+public class SymbolTable {
+}
